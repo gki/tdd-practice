733e5bf8c53ee6d29fce5a2e6d94addd5b221d84
Gen Takeda <hogehoge.gki@gmail.com>
2020-11-22 19:57:00 +0900

    add test and code for Franc

--- typescript_node/src/money.ts
+++ typescript_node/src/money.ts
@@ -13,3 +13,19 @@ export class Dollar {
     return this.amount === other.amount;
   }
 }
+
+export class Franc {
+  amount: number;
+
+  constructor(initialAmount: number) {
+    this.amount = initialAmount;
+  }
+
+  times(n: number): Franc {
+    return new Franc(this.amount * n);
+  }
+
+  equals(other: Franc): boolean {
+    return this.amount === other.amount;
+  }
+}
--- typescript_node/test/money.test.ts
+++ typescript_node/test/money.test.ts
@@ -1,4 +1,4 @@
-import { Dollar } from "../src/money";
+import { Dollar, Franc } from "../src/money";
 
 describe("money test", () => {
   test.todo("$5+10CHF=10 (レートが2:1の場合)");
@@ -8,11 +8,22 @@ describe("money test", () => {
     expect(five.times(3)).toStrictEqual(new Dollar(15));
   });
 
-  test.todo("amountをprivateにする");
+  test("5CHF*2=10CHF", () => {
+    const five: Franc = new Franc(5);
+    expect(five.times(2)).toStrictEqual(new Franc(10));
+    expect(five.times(3)).toStrictEqual(new Franc(15));
+  });
+
+  test.todo("nullとの比較");
+  test.todo("他クラスとの比較");
   test.todo("Moneyの丸め処理をどうするか");
   test("equals", () => {
     expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
     expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
   });
   test.todo("hashcode");
+  test.todo("DollarとFrancの重複");
+
+  test.todo("equalsの一般化");
+  test.todo("hashcodeの一般化");
 });
