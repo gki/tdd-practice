2617d1bb187b0cca3da2399643cd8ca535745821
Gen Takeda <hogehoge.gki@gmail.com>
2020-11-23 16:13:05 +0900

    update tests

--- typescript_node/test/money.test.ts
+++ typescript_node/test/money.test.ts
@@ -1,4 +1,6 @@
-import { Money, Bank, Expression } from "../src/money";
+import { Bank } from "../src/bank";
+import { Expression, Sum } from "../src/expression";
+import { Money } from "../src/money";
 
 describe("money test", () => {
   test.todo("$5+10CHF=10 (レートが2:1の場合)");
@@ -17,22 +19,35 @@ describe("money test", () => {
     expect(five.times(3)).toEqual(Money.dollar(15));
   });
 
+  test("plusはSumを返却する", () => {
+    const five: Money = Money.dollar(5);
+    const sum: Sum = five.plus(five);
+    expect(sum.augend).toEqual(five);
+    expect(sum.addend).toEqual(five);
+  });
+
+  test("reduceで加算する", () => {
+    const sum = new Sum(Money.dollar(3), Money.dollar(4));
+    const bank = new Bank();
+    const result: Money = bank.reduce(sum, "USD");
+    expect(result).toEqual(Money.dollar(7));
+  });
+
   test("通貨のテスト", () => {
     expect(Money.dollar(1).currency()).toBe("USD");
     expect(Money.franc(1).currency()).toBe("CHF");
   });
 
-  test.todo("nullとの比較");
-  test.todo("他クラスとの比較");
-  test.todo("Moneyの丸め処理をどうするか");
   test("equals", () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
     expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
     expect(Money.franc(5).equals(Money.dollar(5))).toBe(false);
   });
 
-  test.todo("hashcode");
-  test.todo("DollarとFrancの重複");
-
-  test.todo("hashcodeの一般化");
+  test.todo("$5+$5がMoneyをかえす");
+  test("Bank.reduce(Money)", () => {
+    const bank = new Bank();
+    const result = bank.reduce(Money.dollar(1), "USD");
+    expect(result).toEqual(Money.dollar(1));
+  });
 });
